0358856ba5b20f34abbe7c14f80a6bc50980579c
Kratika Sharma <kratika.k.cpmeena@gmail.com>
2025-09-30 04:43:20 +0530

    moongoose connect updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 app.use(express.json())
 
-mongoose.connect("process.env.MONGODB_URI")
+mongoose.connect(process.env.MONGODB_URI)
 
 const adminSchema = new mongoose.Schema({
     username: String,
