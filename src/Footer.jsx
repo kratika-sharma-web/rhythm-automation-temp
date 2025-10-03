161c0836c54345a687a22be685ce5b775d7c9ce7
Kratika Sharma <kratika.k.cpmeena@gmail.com>
2025-10-04 02:23:26 +0530

    footer update with link

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
 function Footer() {
+    const navigate = useNavigate()
+    const adminLinkClick = () => {
+        navigate('/ra-admin')
+    }
     return (
         <>
             <footer className="bg-dark text-light text-center pt-3 pb-1">
                 <div className="container">
                     <p>
-                        &copy; 2023 Copyright <a href="/ra-admin" style={{ fontWeight: "bold", color: "inherit", textDecoration: "none" }}>@Rhythm Automation</a>
+                        &copy; 2023 Copyright <a href="#" onClick={adminLinkClick} style={{ fontWeight: "bold", color: "inherit", textDecoration: "none" }}>@Rhythm Automation</a>
                     </p>
                 </div>
             </footer>
