6d36085efd5a8352e2175da3621cd70494f2a131
Kratika Sharma <kratika.k.cpmeena@gmail.com>
2025-10-04 02:17:24 +0530

    footer update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,8 +4,7 @@ function Footer() {
             <footer className="bg-dark text-light text-center pt-3 pb-1">
                 <div className="container">
                     <p>
-                        &copy; 2023 Copyright @Rhythm Automation
-                        {/* <a href="/ra-admin" style={{ fontWeight: "bold", color: "inherit", textDecoration: "none" }}>@Rhythm Automation</a> */}
+                        &copy; 2023 Copyright <a href="/ra-admin" style={{ fontWeight: "bold", color: "inherit", textDecoration: "none" }}>@Rhythm Automation</a>
                     </p>
                 </div>
             </footer>
